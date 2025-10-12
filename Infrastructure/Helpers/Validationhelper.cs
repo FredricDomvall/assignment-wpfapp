6d5ac232566811d7e3ff6978d0a02ba896641e93
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 23:27:39 +0200

    added string validation helper to check for invalid or empty strings

--- Infrastructure/Helpers/Validationhelper.cs
+++ Infrastructure/Helpers/Validationhelper.cs
@@ -1,4 +1,17 @@
-﻿namespace Infrastructure.Helpers;
-public class Validationhelper
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Helpers;
+public static class Validationhelper
 {
+    public static AnswerOutcome<bool> ValidateString(string stringInputs, List<Product> productList)
+    {
+        if (string.IsNullOrWhiteSpace(stringInputs))
+            return new AnswerOutcome<bool> { Statement = false, Answer = "\tText-inputs can not be left empty" };
+        if (stringInputs.Length < 3)
+            return new AnswerOutcome<bool> { Statement = false, Answer = "\tText-inputs be at least 3 characters long." };
+        if (stringInputs.Any(char.IsDigit))
+            return new AnswerOutcome<bool> { Statement = false, Answer = "\tText-inputs not contain numbers." };
+
+        return new AnswerOutcome<bool> { Statement = true };
+    }
 }
