e6fcc6c69e81b73a38cca84f056fdc5c03690190
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 17:16:49 +0200

    updated productmodel

--- Infrastructure/Models/Product.cs
+++ Infrastructure/Models/Product.cs
@@ -4,4 +4,6 @@ public class Product
     public Guid ProductId { get; set; }
     public string ProductName { get; set; } = null!;
     public decimal ProductPrice { get; set; }
+    public Category Category { get; set; } = null!;
+    public Manufacturer Manufacturer { get; set; } = null!;
 }
