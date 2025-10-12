cd35e2f4929397cf2151d7440694e8b2b735e96f
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 13:40:57 +0200

    interface for productservice created with 4 methods

---
+++ Infrastructure/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces;
+public interface IProductService
+{
+    bool AddProductToList(ProductForm productForm);
+    IEnumerable<Product> GetAllProductsFromList();
+    IEnumerable<Product> LoadListFromFile();
+    bool SaveListToFile();
+
+
+}
