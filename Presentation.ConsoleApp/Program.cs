edaf778626fde958c194008e4860581c5a950926
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 21:46:21 +0200

    making JsonfileRepository taking type on program

--- Presentation.ConsoleApp/Program.cs
+++ Presentation.ConsoleApp/Program.cs
@@ -1,10 +1,11 @@
-﻿using Infrastructure.Repositories;
+﻿using Infrastructure.Models;
+using Infrastructure.Repositories;
 using Infrastructure.Services;
 using Presentation.ConsoleApp.Menus;
 
 string jsonFileSource = "products.json";
 
-JsonFileRepository jsonFileRepository = new JsonFileRepository(jsonFileSource);
+JsonFileRepository<Product> jsonFileRepository = new JsonFileRepository<Product>(jsonFileSource);
 ProductService productService = new ProductService(jsonFileRepository);
 ProductMenu productMenu = new ProductMenu(productService);
 MainMenu mainMenu = new MainMenu(productMenu);
