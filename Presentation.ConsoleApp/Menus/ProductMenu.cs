b8170b2bb8fff8e784f562f224f2782c77969461
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 15:15:20 +0200

    implementing DisplayProductMenu in ProductMenu

--- Presentation.ConsoleApp/Menus/ProductMenu.cs
+++ Presentation.ConsoleApp/Menus/ProductMenu.cs
@@ -1,4 +1,26 @@
 ﻿namespace Presentation.ConsoleApp.Menus;
 internal class ProductMenu
 {
+    public void DisplayProductMenu()
+    {
+        Console.WriteLine("Product Menu");
+        Console.WriteLine("1. List Products");
+        Console.WriteLine("2. Add Product");
+        Console.WriteLine("0. Back to Main Menu");
+        Console.Write("Select an option: ");
+        var option = Console.ReadLine();
+        switch (option)
+        {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "0":
+                return;
+            default:
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+        }
+        DisplayProductMenu();
+    }
 }
