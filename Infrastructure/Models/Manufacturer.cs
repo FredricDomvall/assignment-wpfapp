c53008c7888968dc4c95da4eaf2fd54d8ff602ae
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 17:10:56 +0200

    added Manufacturer model

---
+++ Infrastructure/Models/Manufacturer.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Models;
+public class Manufacturer
+{
+    public Guid ManufacturerId { get; set; }
+    public string ManufacturerName { get; set; } = null!;
+    public string ManufacturerCountry { get; set; } = null!;
+    public string ManufacturerEmail { get; set; } = null!;
+}
