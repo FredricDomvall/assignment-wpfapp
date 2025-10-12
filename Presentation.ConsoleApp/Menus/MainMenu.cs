22263c7abbab1e36be7cba6ec82c12b234dfa73a
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 15:08:36 +0200

    implement DisplayMainMenu method

--- Presentation.ConsoleApp/Menus/MainMenu.cs
+++ Presentation.ConsoleApp/Menus/MainMenu.cs
@@ -1,4 +1,26 @@
 ﻿namespace Presentation.ConsoleApp.Menus;
 internal class MainMenu
 {
+    private void DisplayMainMenu()
+    {
+        Console.WriteLine("Main Menu");
+        Console.WriteLine("1. Product Menu");
+        Console.WriteLine("0. Exit");
+        Console.Write("Select an option: ");
+        var option = Console.ReadLine();
+        switch (option)
+        {
+            case "1":
+                var productMenu = new ProductMenu();
+                //productMenu.DisplayProductMenu();
+                break;
+            case "0":
+                //ExitApplication();
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+        }
+        DisplayMainMenu();
+    }
 }
