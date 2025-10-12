9f255b5a965ae024b42709eddf213bfb415eb886
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 12:54:39 +0200

    productmodel created

---
+++ Infrastructure/Models/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Models;
+public class Product
+{
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = null!;
+    public decimal ProductPrice { get; set; }
+}
