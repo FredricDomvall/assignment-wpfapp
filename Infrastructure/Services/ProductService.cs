4c4a8ae6f9597dd956eb3eb25978f54039585b55
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 13:43:14 +0200

    created productservice and implementing interface

---
+++ Infrastructure/Services/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Interfaces;
+using Infrastructure.Models;
+
+namespace Infrastructure.Services;
+public class ProductService : IProductService
+{
+    public bool AddProductToList(ProductForm productForm)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> GetAllProductsFromList()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> LoadListFromFile()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool SaveListToFile()
+    {
+        throw new NotImplementedException();
+    }
+}
