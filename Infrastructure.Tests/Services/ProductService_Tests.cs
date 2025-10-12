f4ce23623763c6d489a9041ec8b417ba1afcd289
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 16:18:13 +0200

    testmethod created

--- Infrastructure.Tests/Services/ProductService_Tests.cs
+++ Infrastructure.Tests/Services/ProductService_Tests.cs
@@ -1,4 +1,13 @@
 ﻿namespace Infrastructure.Tests.Services;
 public class ProductService_Tests
 {
+    [Fact]
+    public void AddProductToList_ShouldAddProductToList_WhenValidInput()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
 }
