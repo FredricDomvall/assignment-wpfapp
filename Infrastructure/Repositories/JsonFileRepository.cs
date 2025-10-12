3d657cd28754423c4331f30ea014180be50b31d4
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 13:11:00 +0200

    creating jsonfilerepository and implementing interface

---
+++ Infrastructure/Repositories/JsonFileRepository.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Interfaces;
+using Infrastructure.Models;
+
+namespace Infrastructure.Repositories;
+public class JsonFileRepository : IJsonFileRepository
+{
+    public List<Product> ReadFromJsonFile()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool WriteToJsonFile(List<Product> productList)
+    {
+        throw new NotImplementedException();
+    }
+}
