8254768eb3692a1273de7af40a552101d6e188b2
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-12 22:11:51 +0200

    creating Manufacturer interface

---
+++ Infrastructure/Interfaces/IManufacturerService.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces;
+public interface IManufacturerService
+{
+    Task<AnswerOutcome<Manufacturer>> AddManufacturerToListAsync(Manufacturer manufacturer);
+    Task<AnswerOutcome<IEnumerable<Manufacturer>>> GetAllManufacturersFromListAsync();
+    Task<AnswerOutcome<IEnumerable<Manufacturer>>> LoadListFromFileAsync();
+    Task<AnswerOutcome<bool>> SaveListToFileAsync();
+}
