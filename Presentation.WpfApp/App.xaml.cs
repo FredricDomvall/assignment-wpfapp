e8a804e785c7461d0884ed5a1531cb9406e8619b
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-13 17:26:07 +0200

    cleaning app.xaml.cs

--- Presentation.WpfApp/App.xaml.cs
+++ Presentation.WpfApp/App.xaml.cs
@@ -1,28 +1,21 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Presentation.WpfApp.ViewModels;
-using System.Configuration;
-using System.Data;
 using System.Windows;
 
-namespace Presentation.WpfApp
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-        private IHost _host;
-        public App()
-        {
-            _host = Host.CreateDefaultBuilder()
-                .ConfigureServices(services => 
-                {
-                    services.AddSingleton<MainViewModel>();
-                    services.AddSingleton<MainWindow>();
-                })
-                .Build();
-        }
-    }
+namespace Presentation.WpfApp;
 
+public partial class App : Application
+{
+    private IHost _host;
+    public App()
+    {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services => 
+            {
+                services.AddSingleton<MainViewModel>();
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+    }
 }
