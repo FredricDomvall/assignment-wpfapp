837cbd4b1a219708304aaf4a0af5d0ba4e9497fe
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-13 18:22:23 +0200

    creating viewmodels folderstructure and StartViewModel

---
+++ Presentation.WpfApp/ViewModels/StartViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Presentation.WpfApp.ViewModels;
+
+public partial class StartViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _title = "Start View Model";
+}
