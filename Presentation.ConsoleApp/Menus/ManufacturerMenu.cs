be7b0c3e708e9078ad12ea4b53d3ae44ea236a16
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-13 13:43:33 +0200

    adding manufacturer DI to constructor in manufacturerMenu

--- Presentation.ConsoleApp/Menus/ManufacturerMenu.cs
+++ Presentation.ConsoleApp/Menus/ManufacturerMenu.cs
@@ -1,5 +1,12 @@
-﻿namespace Presentation.ConsoleApp.Menus;
+﻿using Infrastructure.Interfaces;
+
+namespace Presentation.ConsoleApp.Menus;
 public class ManufacturerMenu
 {
+    private readonly IManufacturerService _manufacturerService;
+    public ManufacturerMenu(IManufacturerService manufacturerService)
+    {
+        _manufacturerService = manufacturerService;
+    }
 
 }
