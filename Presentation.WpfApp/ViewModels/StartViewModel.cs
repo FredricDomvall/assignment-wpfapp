8a0b925dac1006641be648bebc4d0948458df254
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-13 18:27:23 +0200

    creating IServiceprovider and ctor in StartViewModel

--- Presentation.WpfApp/ViewModels/StartViewModel.cs
+++ Presentation.WpfApp/ViewModels/StartViewModel.cs
@@ -4,6 +4,11 @@ namespace Presentation.WpfApp.ViewModels;
 
 public partial class StartViewModel : ObservableObject
 {
+    private readonly IServiceProvider _serviceProvider;
+    public StartViewModel(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
     [ObservableProperty]
     private string _title = "Start View Model";
 }
