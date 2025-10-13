179bbaf9876223ed394192b0112f1fd929e2052f
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-14 01:07:23 +0200

    implement navigatetoproductcreateview method & bind button

--- Presentation.WpfApp/ViewModels/ProductViewModels/ProductListViewModel.cs
+++ Presentation.WpfApp/ViewModels/ProductViewModels/ProductListViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Presentation.WpfApp.ViewModels.ProductViewModels;
 public partial class ProductListViewModel : ObservableObject
@@ -10,4 +12,12 @@ public partial class ProductListViewModel : ObservableObject
     }
     [ObservableProperty]
     private string _title = "Product List";
+
+    [RelayCommand]
+    private void NavigateToProductCreateView()
+    {
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        var productCreateViewModel = _serviceProvider.GetRequiredService<ProductCreateViewModel>();
+        mainViewModel.CurrentViewModel = productCreateViewModel;
+    }
 }
