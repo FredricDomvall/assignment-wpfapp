c50da5241af079d6da7cb1a3fd75d579040ffa88
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-13 13:44:28 +0200

    adding manufacturermenu run and display menu methods

--- Presentation.ConsoleApp/Menus/ManufacturerMenu.cs
+++ Presentation.ConsoleApp/Menus/ManufacturerMenu.cs
@@ -8,5 +8,34 @@ public class ManufacturerMenu
     {
         _manufacturerService = manufacturerService;
     }
+    public async Task Run()
+    {
+        await DisplayManufacturerMenu();
+    }
+    private async Task DisplayManufacturerMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("=== MANUFACTURER MENU ===");
+        Console.WriteLine("1. List Manufacturers");
+        Console.WriteLine("2. Add Manufacturer");
+        Console.WriteLine("0. Back to Main Menu");
+        Console.Write("Select an option: ");
+        var option = Console.ReadLine();
+        switch (option)
+        {
+            case "1":
+                //await ShowManufacturersInList();
+                break;
+            case "2":
+                //await AddNewManufacturerToList();
+                break;
+            case "0":
+                return;
+            default:
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+        }
+        await DisplayManufacturerMenu();
+    }
 
 }
