2c0e73f73c88a901c317d93f39f92ecfc76efdfc
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-13 18:58:28 +0200

    registering all viewmodels in app.xaml.cs Di builder

--- Presentation.WpfApp/App.xaml.cs
+++ Presentation.WpfApp/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Presentation.WpfApp.ViewModels;
+using Presentation.WpfApp.ViewModels.CategoryViewModels;
+using Presentation.WpfApp.ViewModels.ManufacturerViewModels;
+using Presentation.WpfApp.ViewModels.ProductViewModels;
 using System.Windows;
 
 namespace Presentation.WpfApp;
@@ -15,6 +18,24 @@ public partial class App : Application
             {
                 services.AddSingleton<MainViewModel>();
                 services.AddSingleton<MainWindow>();
+
+                services.AddTransient<StartViewModel>();
+
+                services.AddTransient<ProductCreateViewModel>();
+                services.AddTransient<ProductListViewModel>();
+                services.AddTransient<ProductUpdateViewModel>();
+                services.AddTransient<ProductDeleteViewModel>();
+
+                services.AddTransient<CategoryCreateViewModel>();
+                services.AddTransient<CategoryListViewModel>();
+                services.AddTransient<CategoryUpdateViewModel>();
+                services.AddTransient<CategoryDeleteViewModel>();
+
+                services.AddTransient<ManufacturerCreateViewModel>();
+                services.AddTransient<ManufacturerListViewModel>();
+                services.AddTransient<ManufacturerUpdateViewModel>();
+                services.AddTransient<ManufacturerDeleteViewModel>();
+
             })
             .Build();
     }
