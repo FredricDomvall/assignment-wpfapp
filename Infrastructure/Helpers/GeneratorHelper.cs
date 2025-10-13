327eaddc1e128a06cad52f5721547a1892c68152
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-13 05:23:08 +0200

    optimizing ArticleNumberGenerator Helper Method

--- Infrastructure/Helpers/GeneratorHelper.cs
+++ Infrastructure/Helpers/GeneratorHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace Infrastructure.Helpers;
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Helpers;
 public static class GeneratorHelper
 {
     public static Guid GenerateGuidId() => Guid.NewGuid();
@@ -6,10 +8,18 @@ public static class GeneratorHelper
     {
         return categoryName.Trim().ToUpper().Substring(0,3);
     }
-    public static string GenerateArticleNumber(string prefix)
+    public static string GenerateArticleNumber(string prefix, List<Product> productList)
     {
-        var random = new Random();
-        var randomDigits = random.Next(0, 99999);
-        return $"{prefix}-{randomDigits.ToString("D5")}";
+        int index = 1;
+        string newArticleNumber;
+
+        do
+        {
+            newArticleNumber = $"{prefix}-{index:D5}";
+            index++;
+        }
+        while (productList.Any(p => p.ProductCode == newArticleNumber));
+
+        return newArticleNumber;
     }
 }
