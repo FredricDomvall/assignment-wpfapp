229c1fbd8185db0be4b600cb1d759e4ccf879ce2
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-13 20:33:52 +0200

    act & assert part of productservice test

--- Infrastructure.Tests/Services/ProductService_Tests.cs
+++ Infrastructure.Tests/Services/ProductService_Tests.cs
@@ -21,33 +21,31 @@ public class ProductService_Tests
             CategoryFileSource = _testFilePath,
             ManufacturerFileSource = _testFilePath
         };
-        var categoryJsonFileRepository = new JsonFileRepository<Category>();
-        var categoryService = new CategoryService(categoryJsonFileRepository, fileSources);
-
+        var productJsonFileRepository = new JsonFileRepository<Product>();
         var manufacturerJsonFileRepository = new JsonFileRepository<Manufacturer>();
+        var categoryJsonFileRepository = new JsonFileRepository<Category>();
+
+        var categoryService = new CategoryService(categoryJsonFileRepository, fileSources);  
         var manufacturerService = new ManufacturerService(manufacturerJsonFileRepository, fileSources);
-        var jsonFileRepository = new JsonFileRepository<Product>();
-        var productService = new ProductService(jsonFileRepository, manufacturerService, categoryService, fileSources);
+
+        IProductService productService = new ProductService(productJsonFileRepository, manufacturerService, categoryService, fileSources);
         var validProduct = new ProductForm
         {
             ProductName = "ValidTestProduct",
-            ProductPrice = "12,34"
-        };
-        var invalidProduct = new ProductForm
-        {
-            ProductName = " ",
-            ProductPrice = "abc"
+            ProductPrice = "12,34",
+            CategoryName = "TestCategory",
+            ManufacturerName = "TestManufacturer",
+            ManufacturerCountry = "TestCountry",
+            ManufacturerEmail = "test@example.com"
         };
 
 
         //Act
         var validResult = await productService.AddProductToListAsync(validProduct);
-        var invalidResult = await productService.AddProductToListAsync(invalidProduct);
         var productExistsInList = await productService.GetAllProductsFromListAsync();
 
         //Assert
         Assert.True(validResult.Statement);
-        Assert.False(invalidResult.Statement);
         Assert.Contains(productExistsInList.Outcome!, p => p.ProductName == "ValidTestProduct" && p.ProductPrice == 12.34m);
         Assert.True(File.Exists(_testFilePath));
     }
