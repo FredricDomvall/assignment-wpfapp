73dd3c6b3aa81d5f50b32b4999b837ebcbfaf0e5
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-14 21:38:54 +0200

    all adaptions completed

--- Presentation.WpfApp/ViewModels/CategoryViewModels/CategoryCreateViewModel.cs
+++ Presentation.WpfApp/ViewModels/CategoryViewModels/CategoryCreateViewModel.cs
@@ -1,13 +1,70 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
 
 namespace Presentation.WpfApp.ViewModels.CategoryViewModels;
 public partial class CategoryCreateViewModel : ObservableObject
 {
     private readonly IServiceProvider _serviceProvider;
-    public CategoryCreateViewModel(IServiceProvider serviceProvider)
+    private readonly IJsonFileRepository<Category> _categoryRepository;
+    private readonly ICategoryService _categoryService;
+
+    public CategoryCreateViewModel(
+        IServiceProvider serviceProvider,
+        IJsonFileRepository<Category> categoryRepository,
+        ICategoryService categoryService)
     {
         _serviceProvider = serviceProvider;
+        _categoryRepository = categoryRepository;
+        _categoryService = categoryService;
     }
+
     [ObservableProperty]
     private string _title = "Create New Category";
+    [ObservableProperty]
+    private ObservableCollection<Category> _categories = new();
+    [ObservableProperty]
+    private Category? _newCategory = new Category();
+
+    private async Task LoadCategoriesAsync()
+    {
+        var loadCategoriesResult = await _categoryService.GetAllCategoriesFromListAsync();
+        if (loadCategoriesResult.Statement is true)
+            Categories = new ObservableCollection<Category>(loadCategoriesResult.Outcome!);
+        else
+            Categories = new ObservableCollection<Category>();
+    }
+
+    [RelayCommand]
+    private async Task RefreshCategories()
+    {
+        await LoadCategoriesAsync();
+    }
+
+    [RelayCommand]
+    private async Task SaveNewCategory()
+    {
+        var addResult = await _categoryService.AddCategoryToListAsync(NewCategory!);
+        if (addResult.Statement is true)
+        {
+            var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+            var categoryListViewModel = _serviceProvider.GetRequiredService<CategoryListViewModel>();
+            mainViewModel.CurrentViewModel = categoryListViewModel;
+        }
+    }
+
+    [RelayCommand]
+    private void CancelNewCategoryCreation()
+    {
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        var categoryListViewModel = _serviceProvider.GetRequiredService<CategoryListViewModel>();
+        mainViewModel.CurrentViewModel = categoryListViewModel;
+    }
+
+    /***********************************************************************************
+     *                          ONLY NAVIGATION METHODS BELOW                          *
+     ***********************************************************************************/
 }
--- Presentation.WpfApp/ViewModels/ManufacturerViewModels/ManufacturerCreateViewModel.cs
+++ Presentation.WpfApp/ViewModels/ManufacturerViewModels/ManufacturerCreateViewModel.cs
@@ -1,13 +1,70 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
 
 namespace Presentation.WpfApp.ViewModels.ManufacturerViewModels;
 public partial class ManufacturerCreateViewModel : ObservableObject
 {
     private readonly IServiceProvider _serviceProvider;
-    public ManufacturerCreateViewModel(IServiceProvider serviceProvider)
+    private readonly IJsonFileRepository<Manufacturer> _manufacturerRepository;
+    private readonly IManufacturerService _manufacturerService;
+
+    public ManufacturerCreateViewModel(
+        IServiceProvider serviceProvider,
+        IJsonFileRepository<Manufacturer> manufacturerRepository,
+        IManufacturerService manufacturerService)
     {
         _serviceProvider = serviceProvider;
+        _manufacturerRepository = manufacturerRepository;
+        _manufacturerService = manufacturerService;
     }
+
     [ObservableProperty]
     private string _title = "Create New Manufacturer";
+    [ObservableProperty]
+    private Manufacturer? _newManufacturer = new Manufacturer();
+    [ObservableProperty]
+    private ObservableCollection<Manufacturer> _manufacturers = new();
+
+    private async Task LoadManufacturersAsync()
+    {
+        var loadManufacturersResult = await _manufacturerService.GetAllManufacturersFromListAsync();
+        if (loadManufacturersResult.Statement is true)
+            Manufacturers = new ObservableCollection<Manufacturer>(loadManufacturersResult.Outcome!);
+        else
+            Manufacturers = new ObservableCollection<Manufacturer>();
+    }
+
+    [RelayCommand]
+    private async Task RefreshManufacturers()
+    {
+        await LoadManufacturersAsync();
+    }
+
+    [RelayCommand]
+    private async Task SaveNewManufacturer()
+    {
+        var addResult = await _manufacturerService.AddManufacturerToListAsync(NewManufacturer!);
+        if (addResult.Statement is true)
+        {
+            var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+            var manufacturerListViewModel = _serviceProvider.GetRequiredService<ManufacturerListViewModel>();
+            mainViewModel.CurrentViewModel = manufacturerListViewModel;
+        }
+    }
+
+    [RelayCommand]
+    private void CancelNewManufacturerCreation()
+    {
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        var manufacturerListViewModel = _serviceProvider.GetRequiredService<ManufacturerListViewModel>();
+        mainViewModel.CurrentViewModel = manufacturerListViewModel;
+    }
+
+    /***********************************************************************************
+     *                          ONLY NAVIGATION METHODS BELOW                          *
+     ***********************************************************************************/
 }
