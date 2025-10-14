52ef35c9ca3a6dfc55cdb21679b7e63c430f9f16
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-14 18:51:13 +0200

    implementing Cancelmethod in productcreateviewmodel + ctor build

--- Presentation.WpfApp/ViewModels/ProductViewModels/ProductCreateViewModel.cs
+++ Presentation.WpfApp/ViewModels/ProductViewModels/ProductCreateViewModel.cs
@@ -1,13 +1,30 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Presentation.WpfApp.ViewModels.ProductViewModels;
 public partial class ProductCreateViewModel : ObservableObject
 {
     private readonly IServiceProvider _serviceProvider;
-    public ProductCreateViewModel(IServiceProvider serviceProvider)
+    private readonly IJsonFileRepository<Product> _productRepository;
+    private readonly IProductService _productService;
+    public ProductCreateViewModel(IServiceProvider serviceProvider, IJsonFileRepository<Product> productRepository, IProductService productService)
     {
         _serviceProvider = serviceProvider;
+        _productRepository = productRepository;
+        _productService = productService;
     }
     [ObservableProperty]
     private string _title = "Create New Product";
+    
+    private void SaveNewProduct()
+    {
+    }
+    private void CancelNewProductCreation()
+    {
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        var productListViewModel = _serviceProvider.GetRequiredService<ProductListViewModel>();
+        mainViewModel.CurrentViewModel = productListViewModel;
+    }
 }
