914b1423224a07e41eade42439e1bc6745f43723
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-15 02:22:06 +0200

    changing property to observvableproperty

--- Presentation.WpfApp/ViewModels/ProductViewModels/ProductUpdateViewModel.cs
+++ Presentation.WpfApp/ViewModels/ProductViewModels/ProductUpdateViewModel.cs
@@ -11,6 +11,6 @@ public partial class ProductUpdateViewModel : ObservableObject
     }
     [ObservableProperty]
     private string _title = "Update Product";
-
-    public Product? CurrentProductDetails { get; internal set; }
+    [ObservableProperty]
+    public Product? _currentProductDetails;
 }
