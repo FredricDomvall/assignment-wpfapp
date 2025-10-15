e4a44d8f81cb4ffef649ff8c7253a555164902c5
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-15 02:54:54 +0200

    adding productcode to Form and implementing UpdateProduct Method

--- Presentation.WpfApp/ViewModels/ProductViewModels/ProductUpdateViewModel.cs
+++ Presentation.WpfApp/ViewModels/ProductViewModels/ProductUpdateViewModel.cs
@@ -1,15 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Infrastructure.Interfaces;
 using Infrastructure.Models;
 using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using System.Xml;
 
 namespace Presentation.WpfApp.ViewModels.ProductViewModels;
 public partial class ProductUpdateViewModel : ObservableObject
 {
     private readonly IServiceProvider _serviceProvider;
-    public ProductUpdateViewModel(IServiceProvider serviceProvider)
+    private readonly IProductService _productService;
+    public ProductUpdateViewModel(IServiceProvider serviceProvider, IProductService productService)
     {
         _serviceProvider = serviceProvider;
+        _productService = productService;
     }
     [ObservableProperty]
     private string _title = "Update Product";
@@ -17,8 +22,26 @@ public partial class ProductUpdateViewModel : ObservableObject
     public Product? _currentProductDetails;
 
     [RelayCommand]
-    private void UpdateProduct()
+    private async Task UpdateProduct()
     {
+        ProductForm productToUpdate = new ProductForm();
+        if (CurrentProductDetails is null)
+        {
+            MessageBox.Show("All Fields Required. Try again!");
+            return;
+        }
+        productToUpdate.ProductName = CurrentProductDetails.ProductName;
+        productToUpdate.ProductPrice = CurrentProductDetails.ProductPrice.ToString();
+        productToUpdate.ProductCode = CurrentProductDetails.ProductCode;
+        productToUpdate.ProductDescription = CurrentProductDetails.ProductDescription;
+        productToUpdate.CategoryName = CurrentProductDetails.Category.CategoryName;
+        productToUpdate.ManufacturerName = CurrentProductDetails.Manufacturer.ManufacturerName;
+        productToUpdate.ManufacturerEmail = CurrentProductDetails.Manufacturer.ManufacturerEmail;
+        productToUpdate.ManufacturerCountry = CurrentProductDetails.Manufacturer.ManufacturerCountry;
 
+        var updateResult = await _productService.UpdateProductInListByIdAsync(CurrentProductDetails.ProductId, productToUpdate);
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        var productListViewModel = _serviceProvider.GetRequiredService<ProductListViewModel>();
+        mainViewModel.CurrentViewModel = productListViewModel;
     }
 }
