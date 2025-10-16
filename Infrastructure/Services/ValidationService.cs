993d1019f79dab420cd5d75d14d86181e770b2d3
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-16 17:20:55 +0200

    basemethod created

---
+++ Infrastructure/Services/ValidationService.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Models;
+using System.Collections.Generic;
+
+namespace Infrastructure.Services;
+
+public class ValidationService
+{
+    private List<AnswerOutcome<bool>> _validationResults = new();
+
+    public AnswerOutcome<List<AnswerOutcome<bool>>> ProductValidationResults(List<AnswerOutcome<bool>> productserviceListResult )
+    {
+
+        return new AnswerOutcome<List<AnswerOutcome<bool>>> { Statement = true, Answer = "Validation results retrieved.", Outcome = productserviceListResult };
+    }
+}
