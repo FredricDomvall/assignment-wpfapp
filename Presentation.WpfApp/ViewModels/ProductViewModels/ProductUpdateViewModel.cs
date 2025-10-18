ff712525eb607da95897f7f677710bc96d1144a9
FredricDomvall <Fredric.domvall@outlook.com>
2025-10-18 06:50:16 +0200

    updating style and implementing other lists to productupdateviewmodel

--- Presentation.WpfApp/ViewModels/ProductViewModels/ProductUpdateViewModel.cs
+++ Presentation.WpfApp/ViewModels/ProductViewModels/ProductUpdateViewModel.cs
@@ -3,23 +3,55 @@ using CommunityToolkit.Mvvm.Input;
 using Infrastructure.Interfaces;
 using Infrastructure.Models;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Xml;
 
 namespace Presentation.WpfApp.ViewModels.ProductViewModels;
 public partial class ProductUpdateViewModel : ObservableObject
 {
     private readonly IServiceProvider _serviceProvider;
     private readonly IProductService _productService;
-    public ProductUpdateViewModel(IServiceProvider serviceProvider, IProductService productService)
+    private readonly ICategoryService _categoryService;
+    private readonly IManufacturerService _manufacturerService;
+    public ProductUpdateViewModel(
+        IServiceProvider serviceProvider,
+        IProductService productService,
+        ICategoryService categoryService,
+        IManufacturerService manufacturerService)
     {
         _serviceProvider = serviceProvider;
         _productService = productService;
+        _categoryService = categoryService;
+        _manufacturerService = manufacturerService;
+        LoadOtherLists();
     }
     [ObservableProperty]
     private string _title = "Update Product";
     [ObservableProperty]
     public Product? _currentProductDetails;
+    [ObservableProperty]
+    private ObservableCollection<Category> _categories = new();
+    [ObservableProperty]
+    private Category? _selectedCategory;
+    [ObservableProperty]
+    private Manufacturer? _selectedManufacturer;
+    [ObservableProperty]
+    private ObservableCollection<Manufacturer> _manufacturers = new();
+
+    private void LoadOtherLists()
+    {
+        var loadCategoriesResult = _categoryService.GetAllCategoriesFromList();
+        if (loadCategoriesResult.Statement is true)
+            Categories = new ObservableCollection<Category>(loadCategoriesResult.Outcome!);
+        else
+            Categories = new ObservableCollection<Category>();
+
+        var loadManufacturersResult = _manufacturerService.GetAllManufacturersFromList();
+        if (loadManufacturersResult.Statement is true)
+            Manufacturers = new ObservableCollection<Manufacturer>(loadManufacturersResult.Outcome!);
+        else
+            Manufacturers = new ObservableCollection<Manufacturer>();
+    }
 
     [RelayCommand]
     private async Task UpdateProduct()
